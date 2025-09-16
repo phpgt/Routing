7568b855b637ab66a2a3e03999c7d4aa1d26be7a
Greg Bowler <greg@g105b.com>
2025-09-16 14:09:45 +0100

    maintenance: reduce cyclomatic complexity of LogicStreamWrapper

--- src/LogicStream/LogicStreamWrapper.php
+++ src/LogicStream/LogicStreamWrapper.php
@@ -12,6 +12,7 @@ class LogicStreamWrapper {
 	private string $path;
 	private string $contents;
 
+	// phpcs:ignore Generic.NamingConventions.CamelCapsFunctionName
 	function stream_open(string $path):bool {
 		$this->position = 0;
 		$this->path = substr($path, strpos($path, "//") + 2);
@@ -20,25 +21,30 @@ class LogicStreamWrapper {
 		return true;
 	}
 
+	// phpcs:ignore Generic.NamingConventions.CamelCapsFunctionName
 	function stream_read(int $count):string {
 		$ret = substr($this->contents, $this->position, $count);
 		$this->position += strlen($ret);
 		return $ret;
 	}
 
+	// phpcs:ignore Generic.NamingConventions.CamelCapsFunctionName
 	function stream_tell():int {
 		return $this->position;
 	}
 
+	// phpcs:ignore Generic.NamingConventions.CamelCapsFunctionName
 	function stream_eof():bool {
 		return $this->position >= strlen($this->contents);
 	}
 
+	// phpcs:ignore Generic.NamingConventions.CamelCapsFunctionName
 	public function stream_set_option():bool {
 		return false;
 	}
 
 	/** @return array<int, string> File statistics, as per stat(). */
+	// phpcs:ignore Generic.NamingConventions.CamelCapsFunctionName
 	public function stream_stat():array {
 		return [];
 	}
@@ -56,50 +62,79 @@ class LogicStreamWrapper {
 	 */
 	private function loadContents(SplFileObject $file):void {
 		$foundNamespace = false;
-		$withinBlockComment = false;
-		$lineNumber = 0;
+		$this->initFileParsing($file);
 
 		while(!$file->eof() && !$foundNamespace) {
 			$line = $file->fgets();
-			if($lineNumber === 0) {
-// TODO: Allow hashbangs before <?php
-// Maybe this is possible by just skipping while the first character is a hash, and not increasing the line number.
-				if(!str_starts_with($line, "<?php")) {
-					throw new Exception("Logic file at " . $this->path . " must start by opening a PHP tag. See https://www.php.gt/routing/logic-stream-wrapper");
-				}
-			}
-			$trimmedLine = trim($line);
-
-			if(str_starts_with($trimmedLine, "/*")) {
-				$withinBlockComment = true;
-			}
-
-			if($withinBlockComment) {
-				if(str_contains($trimmedLine, "*/")) {
-					$withinBlockComment = false;
-				}
-			}
-			elseif($lineNumber > 0) {
-				if(str_starts_with($trimmedLine, "namespace")) {
-					$foundNamespace = true;
-				}
-				elseif($trimmedLine ) {
-					$namespace = new LogicStreamNamespace(
-						$this->path,
-						self::NAMESPACE_PREFIX
-					);
-					$this->contents .= "namespace $namespace;\t";
-					$foundNamespace = true;
-				}
-			}
-
-			$this->contents .= $line;
-			$lineNumber++;
+			$foundNamespace = $this->processLine($line);
 		}
 
-		while(!$file->eof()) {
-			$line = $file->fgets();
+		// Append the remaining file content
+		$this->appendRemainingFileContent($file);
+	}
+
+	private function initFileParsing(SplFileObject $file):void {
+		$line = $file->fgets();
+		if(!str_starts_with($line, "<?php")) {
+			throw new Exception(
+				"Logic file at " . $this->path . " must start by opening a PHP tag. " .
+				"See https://www.php.gt/routing/logic-stream-wrapper"
+			);
+		}
+		$this->contents .= $line;
+	}
+
+	private function processLine(string $line):bool {
+		static $withinBlockComment = false;
+		static $lineNumber = 0;
+
+		$trimmedLine = trim($line);
+		$lineNumber++;
+
+		if($this->startsBlockComment($trimmedLine)) {
+			$withinBlockComment = true;
+		}
+		if($withinBlockComment) {
+			$withinBlockComment = !$this->endsBlockComment($trimmedLine);
 			$this->contents .= $line;
+			return false;
+		}
+
+		return $this->checkAndAppendNamespace($trimmedLine, $lineNumber, $line);
+	}
+
+	private function startsBlockComment(string $line):bool {
+		return str_starts_with($line, "/*");
+	}
+
+	private function endsBlockComment(string $line):bool {
+		return str_contains($line, "*/");
+	}
+
+	private function checkAndAppendNamespace(
+		string $line,
+		int $lineNumber,
+		string $originalLine,
+	):bool {
+		if($lineNumber > 0) {
+			if(str_starts_with($line, "namespace")) {
+				$this->contents .= $originalLine;
+				return true;
+			}
+			if($line) {
+				$namespace = new LogicStreamNamespace($this->path, self::NAMESPACE_PREFIX);
+				$this->contents .= "namespace $namespace;\t";
+				return true;
+			}
+		}
+
+		$this->contents .= $originalLine;
+		return false;
+	}
+
+	private function appendRemainingFileContent(SplFileObject $file):void {
+		while(!$file->eof()) {
+			$this->contents .= $file->fgets();
 		}
 	}
 }
